f812a64604f5205d21ad693b651764200a23bd97
cdpaiva <cdpaivaa@gmail.com>
2022-05-04 12:27:03 -0400

    Embed analytics

---
+++ analytics.js
@@ -0,0 +1,6 @@
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-LDKM6H7G23', { 'cookieFlags': 'SameSite=None; Secure' });
