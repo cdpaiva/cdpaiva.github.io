d51972a91c91cc511b90b908548b134a11b51c52
cdpaiva <cdpaivaa@gmail.com>
2022-05-22 21:33:27 -0400

    Update for analytics

---
+++ assets/notes_git-aliases.md.c0cafe72.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as i}from"./app.0d2d8b93.js";const u='{"title":"Git Aliases","description":"","frontmatter":{},"headers":[],"relativePath":"notes/git-aliases.md"}',s={},o=i("",12),n=[o];function l(r,c,d,m,g,h){return a(),t("div",null,n)}var _=e(s,[["render",l]]);export{u as __pageData,_ as default};
